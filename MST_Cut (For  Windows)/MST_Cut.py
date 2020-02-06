770105583244248ce351cf1e95b10f1a8487dfa1
Tae1ForAll <60103724+Tae1ForAll@users.noreply.github.com>
2020-02-06 16:26:26 +0700

    Add files via upload

--- MST_Cut (For  Windows)/MST_Cut.py
+++ MST_Cut (For  Windows)/MST_Cut.py
@@ -5,6 +5,7 @@
 
 
 import random
+import pandas as pd
 from pythainlp import word_tokenize as wt
 from pythainlp import pos_tag as pt
 
@@ -18,7 +19,7 @@ from pythainlp import pos_tag as pt
 
 
 def Modify_Data(tokenize_engine):
-    y = open(r'StartWord.txt', 'r', encoding="utf8")
+    y = open('StartWord.txt', 'r')
     Data = y.read()
     Data_Split = Data.split("|")
     Start_Word_Dicts = {1:[],2:[],3:[],4:[],5:[]}
@@ -64,11 +65,54 @@ def Modify_Text(text,Lower_Bound,Upper_Bound,tokenize_engine):
     return Array_Of_Text,Real_len_Array
 
 
+# ### Check_Size_Of_Sentence
+
+# In[5]:
+
+
+def Check_Size_Of_Sentence(text, lenght, tokenize_engine):
+    if len(wt(text,engine=tokenize_engine)) <= 50:
+        return True
+    else:
+        return False
+
+
+# ## Search_Line
+# - ไล่หาเว้นบรรทัด
+
+# In[6]:
+
+
+def Search_Line(Array_Of_Text):
+    Line_Target = []
+    for n in range(0,len(Array_Of_Text)):
+        if Array_Of_Text[n] in ["\n"] and (n-4) >= 0 and (n+4) >= 0 :
+            Line_Target.append(n)
+    return Line_Target
+
+
+# ## Choose_Best_Line
+# - เลือกหาเว้นบรรทัดมา 1 ค่า
+
+# In[7]:
+
+
+def Choose_Best_Line(Line_Target,boundary):
+    print(boundary)
+    if len(Line_Target) > 1:
+        Fin_Case = [random.choice(Line_Target)]
+    elif len(Line_Target) == 1:
+        Fin_Case = Line_Target
+    elif len(Line_Target) == 0:
+        Fin_Case = [boundary]
+    return Fin_Case
+
+
 # # Search_Space
 # 
 # - ไล่หาเว้นวรรค
 
-# In[5]:
+# In[8]:
 
 
 def Search_Space(Array_Of_Text):
@@ -83,7 +127,7 @@ def Search_Space(Array_Of_Text):
 # 
 # - หากกรณีที่มี Space หลายตัว จะเริ่มกระบวนการหา Space ที่ดีที่สุด ตามเคสที่กำหนด
 
-# In[6]:
+# In[9]:
 
 
 def Tag_Prio_Each_SP(Array_Of_Text,SP_Target,Start_Word_Dicts):
@@ -139,7 +183,7 @@ def Tag_Prio_Each_SP(Array_Of_Text,SP_Target,Start_Word_Dicts):
 # 
 # - เลือกหา space มาหนึ่งค่า
 
-# In[7]:
+# In[10]:
 
 
 def Choose_Best_SP(SP_Case_Target,boundary):
@@ -156,7 +200,7 @@ def Choose_Best_SP(SP_Case_Target,boundary):
 # 
 # - กรณีที่ไม่เจอ SPACE เลย จะทำการหาคำเชื่อมแทน
 
-# In[8]:
+# In[11]:
 
 
 def Search_CONJ(Array_Of_Text,Start_Word_Dicts):
@@ -177,7 +221,6 @@ def Search_CONJ(Array_Of_Text,Start_Word_Dicts):
             Case_CONJ_2.append(n-1)
         elif n1_N in Start_Word_Dicts[1]:
             Case_CONJ_1.append(n-1)
-            print(Case_CONJ_1)
     if Case_CONJ_4 != []:
         return Case_CONJ_4
     elif Case_CONJ_3 != []:
@@ -196,7 +239,7 @@ def Search_CONJ(Array_Of_Text,Start_Word_Dicts):
 # 
 # - เลือกหา CONJ มาหนึ่งค่า
 
-# In[9]:
+# In[12]:
 
 
 def Choose_Best_CONJ(Case_CONJ,boundary):
@@ -213,7 +256,7 @@ def Choose_Best_CONJ(Case_CONJ,boundary):
 # 
 # - เป็นฟังก์สำหรับการตัด Sentence
 
-# In[10]:
+# In[13]:
 
 
 def Cut_Sentence(text,Fin_Case,Real_len_Array,tokenize_engine):
@@ -227,7 +270,7 @@ def Cut_Sentence(text,Fin_Case,Real_len_Array,tokenize_engine):
 # 
 # - มาตัดประโยคแล้ว จะนำระยะที่ใช้ตัดมาสร้างประโยคใหม่
 
-# In[11]:
+# In[14]:
 
 
 def New_Sentence(Size_Cut,List_Text,Last_Sentence,lenght):
@@ -238,32 +281,113 @@ def New_Sentence(Size_Cut,List_Text,Last_Sentence,lenght):
     return New_text,Last_Sentence
 
 
+# # Create_List_Of_Keyword
+# - เก็บ Keyword ต่างๆแยกไว้ใน List
+
+# In[15]:
+
+
+def Create_List_Of_Keyword():
+    data = pd.read_excel('NLP.xlsx', sheet_name = "Sentiment")
+    Positive = (data['Positive'].dropna()).tolist()
+    Negative = (data['Negative'].dropna()).tolist()
+    Neutral = (data['Neutral'].dropna()).tolist()
+    Negation = (data['Negation (-)'].dropna()).tolist()
+    return Positive,Negative,Neutral,Negation
+
+
+# # Check_Sentiment_By_Keyword
+# - เช็ค Sentiment ที่อยู่ในประโยค
+
+# In[16]:
+
+
+def Check_Sentimenet_By_Keyword(Positive,Negative,Neutral,Negation,Output_Sentence):
+    Mytext = Output_Sentence
+    Mytext = Mytext.replace(' ','')
+    for n in Neutral:
+        Mytext = Mytext.replace(n,'|Neutral|')
+    for n in Negative:
+        Mytext = Mytext.replace(n,'|Negative|')
+    for n in Positive:
+        Mytext = Mytext.replace(n,'|Positive|')
+    for n in Negation:
+        Mytext = Mytext.replace(n,'|Negation|')
+    Mytext = Mytext.replace('|Negative||Negation|','|Positive|')
+    Mytext = Mytext.replace('|Negation||Negative|','|Positive|')
+    Mytext = Mytext.replace('|Positive||Negation|','|Negative|')
+    Mytext = Mytext.replace('|Negation||Positive|','|Negative|')
+    Neu_Count = len(Mytext.split('|Neutral|'))-1
+    Neg_Count = len(Mytext.split('|Negative|'))-1
+    Pos_Count = len(Mytext.split('|Positive|'))-1
+    return [Pos_Count,Neu_Count,Neg_Count]
+
+
+# # Get_Sentiment 
+# - Count และ Generate Sentiment ที่มีจำนวนมากที่สุด
+
+# In[17]:
+
+
+def Get_Sentiment(STM_Count):
+    y = 0
+    for n in range(0,len(STM_Count)):
+        x = STM_Count[n]
+        if x > y:
+            My_Sentiment = '<'+str(n)+'>'
+            y = x
+        elif x == y:
+            My_Sentiment = '<3>'
+    return My_Sentiment
+
+
 # # sentence 
 # 
 # - เป็นฟังก์ชั่นหลักเพื่อเรียกใช่งาน
 
-# In[12]:
+# In[18]:
 
 
-def sentence(text,lenght,boundary,tokenize_engine):
+def sentence(text,lenght,boundary,tokenize_engine,sentiment):
     Last_Sentence = 0
     New_text = text
     Fin_Sentence = ""
+    My_Check = Check_Size_Of_Sentence(text, lenght, tokenize_engine)
+    if My_Check == True:
+        return text
     while Last_Sentence == 0:
         Start_Word_Dicts = Modify_Data(tokenize_engine)
         Lower_Bound, Upper_Bound = Get_Boundary(lenght,boundary)
         Array_Of_Text,Real_len_Array = Modify_Text(New_text,Lower_Bound,Upper_Bound,tokenize_engine)
         SP_Target = Search_Space(Array_Of_Text)
-        if SP_Target == []:
+        Line_Target = Search_Line(Array_Of_Text)
+        if Line_Target != []: 
+            Fin_Case = Choose_Best_Line(Line_Target,boundary)
+        elif SP_Target == []:
             Case_CONJ = Search_CONJ(Array_Of_Text,Start_Word_Dicts)
-            Fin_Case = Choose_Best_CONJ(Case_CONJ,lenght)
+            Fin_Case = Choose_Best_CONJ(Case_CONJ,boundary)
         else:
             SP_Case_Target,SP_Case = Tag_Prio_Each_SP(Array_Of_Text,SP_Target,Start_Word_Dicts)
             Fin_Case = Choose_Best_SP(SP_Case_Target,boundary)
         Output_Sentence,Size_Cut,List_Text = Cut_Sentence(New_text,Fin_Case,Real_len_Array,tokenize_engine)
         New_text,Last_Sentence = New_Sentence(Size_Cut,List_Text,Last_Sentence,lenght)
-        Output_Sentence += "\n"
+        if sentiment == True:
+            Positive,Negative,Neutral,Negation = Create_List_Of_Keyword()
+            STM_Count = Check_Sentimenet_By_Keyword(Positive,Negative,Neutral,Negation,Output_Sentence)
+            My_Sentiment = Get_Sentiment(STM_Count)
+        else:
+            My_Sentiment = ''
+        if Line_Target != []:
+            Output_Sentence += My_Sentiment+"\n"
+        else :
+            Output_Sentence += My_Sentiment+"\n\n"
         Fin_Sentence += Output_Sentence
     Fin_Sentence += New_text
     return Fin_Sentence
+
+
+# In[ ]:
+
+
+
 
